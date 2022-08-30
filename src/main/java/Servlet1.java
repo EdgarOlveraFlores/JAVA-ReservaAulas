de4dd57c11395c696196a876f67d22e4a7e3b65e
Edgar Olvera Flores <olvera.flores.edgaro@gmail.com>
2022-08-30 18:19:17 -0500

    Pruebas de arquitectura, conexion  con el servidor - web.xml

---
+++ src/main/java/Servlet1.java
@@ -0,0 +1,40 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Servlet1() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(jakarta.servlet.http.HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String parametro1 = request.getParameter("parametro1");
+		System.out.println("Recibiendo Peticion con metodo GET"+ parametro1);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String parametro1 = request.getParameter("parametro1");
+		System.out.println("Recibiendo Peticion con metodo POST");
+	}
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,12 @@
 <html>
 <body>
+
 <h2>Hello Mi mundo!</h2>
+
+<form method="POST" action="Servlet1">
+	<input type="text" value="parametro1"/>
+	<input type="submit" value="Enviar"/>
+</form>
+
 </body>
 </html>
