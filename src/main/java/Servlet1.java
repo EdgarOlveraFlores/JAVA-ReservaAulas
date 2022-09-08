203bf2218b6073ebb16de8d2f72a13ef9a8d50f0
Edgar Olvera Flores <olvera.flores.edgaro@gmail.com>
2022-09-08 12:08:03 -0500

    Actualizacion: correccion de errores de librerias

--- src/main/java/Servlet1.java
+++ src/main/java/Servlet1.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import jakarta.servlet.http.HttpSession;
+
 /**
  * Servlet implementation class Servlet1
  */
@@ -22,16 +24,24 @@ public class Servlet1 extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(jakarta.servlet.http.HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletRequest response) throws jakarta.servlet.ServletException, IOException {
 		// TODO Auto-generated method stub
 		String parametro1 = request.getParameter("parametro1");
+		request.setAttribute("parametro2", "python");
 		System.out.println("Recibiendo Peticion con metodo GET"+ parametro1);
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("parametroSesion", "usuario1");
+		
+		String usuario = (String)session.getAttribute("parametroSesion");
+		
+		System.out.println("Parametro de sesion: " + usuario);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(jakarta.servlet.http.HttpServletRequest request, Servlet1 response) throws jakarta.servlet.ServletException, IOException {
 		// TODO Auto-generated method stub
 		String parametro1 = request.getParameter("parametro1");
 		System.out.println("Recibiendo Peticion con metodo POST");
